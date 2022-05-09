dd8895fb4f954d65df0e83b936d19dbc004931d7
saxenaishii <60481141+saxenaishii@users.noreply.github.com>
2022-05-09 21:29:50 +0530

    Create leftrec.cpp

---
+++ leftrec.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"\nEnter number of non terminals: ";
+    cin>>n;
+    cout<<"\nEnter non terminals one by one: ";
+    int i;
+    vector<string> nonter(n);
+    vector<int> leftrecr(n,0);
+    for(i=0;i<n;++i) {
+            cout<<"\nNon terminal "<<i+1<<" : ";
+        cin>>nonter[i];
+    }
+    vector<vector<string> > prod;
+    cout<<"\nEnter 'esp' for null";
+    for(i=0;i<n;++i) {
+        cout<<"\nNumber of "<<nonter[i]<<" productions: ";
+        int k;
+        cin>>k;
+        int j;
+        cout<<"\nOne by one enter all "<<nonter[i]<<" productions";
+        vector<string> temp(k);
+        for(j=0;j<k;++j) {
+            cout<<"\nRHS of production "<<j+1<<": ";
+            string abc;
+            cin>>abc;
+            temp[j]=abc;
+            if(nonter[i].length()<=abc.length()&&nonter[i].compare(abc.substr(0,nonter[i].length()))==0)
+                leftrecr[i]=1;
+        }
+        prod.push_back(temp);
+    }
+    for(i=0;i<n;++i) {
+        cout<<leftrecr[i];
+    }
+    for(i=0;i<n;++i) {
+        if(leftrecr[i]==0)
+            continue;
+        int j;
+        nonter.push_back(nonter[i]+"'");
+        vector<string> temp;
+        for(j=0;j<prod[i].size();++j) {
+            if(nonter[i].length()<=prod[i][j].length()&&nonter[i].compare(prod[i][j].substr(0,nonter[i].length()))==0) {
+                string abc=prod[i][j].substr(nonter[i].length(),prod[i][j].length()-nonter[i].length())+nonter[i]+"'";
+                temp.push_back(abc);
+                prod[i].erase(prod[i].begin()+j);
+                --j;
+            }
+            else {
+                prod[i][j]+=nonter[i]+"'";
+            }
+        }
+        temp.push_back("esp");
+        prod.push_back(temp);
+    }
+    cout<<"\n\n";
+    cout<<"\nNew set of non-terminals: ";
+    for(i=0;i<nonter.size();++i)
+        cout<<nonter[i]<<" ";
+    cout<<"\n\nNew set of productions: ";
+    for(i=0;i<nonter.size();++i) {
+        int j;
+        for(j=0;j<prod[i].size();++j) {
+            cout<<"\n"<<nonter[i]<<" -> "<<prod[i][j];
+        }
+    }
+    return 0;
+}
+
+
+/*Enter number of non terminals: 3
+
+#Enter non terminals one by one: 
+#Non terminal 1 : E
+#Non terminal 2 : T
+#Non terminal 3 : F
+#Enter 'esp' for null
+#Number of E productions: 2
+#One by one enter all E productions
+#RHS of production 1: E+T
+#RHS of production 2: T
+#Number of T productions: 2
+#One by one enter all T productions
+#RHS of production 1: T*F
+#RHS of production 2: F
+#Number of F productions: 2
+#One by one enter all F productions
+#RHS of production 1: (E)
+#RHS of production 2: i
+
